1952d0436d6bba388b736611b77c24270aa5f11d
JavierFerrer03 <124900827+JavierFerrer03@users.noreply.github.com>
2024-01-16 20:49:19 +0100

    Update conf.php

--- wallapopFerrer/modelos/conf.php
+++ wallapopFerrer/modelos/conf.php
@@ -1,11 +1,18 @@
 <?php 
-    define('MYSQL_USER','root');
-    define('MYSQL_PASS','');
-    define('MYSQL_BD','wallapopFerrer');
-    define('MYSQL_HOST','localhost');
-
-    //define('MYSQL_USER','id21580681_javier');
-    //define('MYSQL_PASS','Xxjopet@sxX65');
-    //define('MYSQL_BD','id21580681_wallapopferrer');
+    //define('MYSQL_USER','root');
+    //define('MYSQL_PASS','');
+    //define('MYSQL_BD','wallapopFerrer');
     //define('MYSQL_HOST','localhost');
+
+    //define('MYSQL_USER','uwprlnzgws');
+    //define('MYSQL_PASS','44S6EK716L1STL65$');
+    //define('MYSQL_BD','wallapopjavierferrer');
+    //define('MYSQL_HOST','wallapopjavierferrer-server.mysql.database.azure.com');
+    
+    $DB_HOST = getenv("DB_HOST");
+    $DB_USER = getenv("DB_USERNAME");
+    $DB_PASS = getenv("DB_PASSWORD");
+    $DB_DB = getenv("DB_NAME");
+
+    $conect = mysqli_connect($BD_HOST, $DB_USER, $DB_PASS, $DB_DB);
 ?>
